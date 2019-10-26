9afdfc4e17d344b5d0f0910f4026cad69d7c3bc2
denaaisyahr <denaaisyah14@gmail.com>
2019-10-26 20:03:44 +0700

    Menambahkan form Tambah Data Mahasiswa

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,9 +1,17 @@
 <div class="container">
+
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <a href="" class="btn btn-primary">Tambah Data Mahasiswa</a>
+        </div>
+    </div>
     <div class="row mt-3">
     <div class="col-md-6">
         <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
-                <li class="list-group-item">Cras justo odio</li>
+                <?php foreach( $mahasiswa as $mhs) : ?>
+                <li class="list-group-item"><?= $mhs['nama']; ?></li>
+<?php endforeach; ?>
             </ul>
         </div>
     </div>
